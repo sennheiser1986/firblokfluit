64b450457428cfa32a3dc4d0381f2b51b648da11
Fionnghall <Fionnghall@998c9d56-e7a0-f005-c44f-48c2c700bf44>
2010-05-07 10:35:50 +0000

    added vhdl stuff

---
+++ alternative/vhdl/Timer.vhd
@@ -0,0 +1,22 @@
+ENTITY Timer IS
+	PORT (
+		SampleClk                 : in std_logic;
+		StartCounter    	  : in boolean;
+		ReachedTarget       : in boolean;
+		SampleTime          : out std_logic;
+		
+	);
+end Timer;
+
+architecture behav of Timer is
+	
+	
+	process(sampleClk)
+	begin
+	if rising_edge(sampleClk) and NOT(ReachedTarget) then
+		if StartCounter then
+			SampleTime <= SampleTime + 1;
+		end if;
+	end if;
+	
+end behav;
